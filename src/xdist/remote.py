b93c1bacf75b45c22d766011e69e79277ae6e4c6
LucasLeRay <lucas.a.leray@gmail.com>
2021-09-20 16:23:24 +0200

    Remove regex in remote

--- src/xdist/remote.py
+++ src/xdist/remote.py
@@ -103,7 +103,6 @@ class WorkerInteractor:
                 except AttributeError:
                     mark = item.get_marker("worker_group")
 
-                # param_value = re.search(r"\[(.*)\]", item.nodeid)
                 if mark:
                     gname = mark.kwargs.get("name")
                     if gname:
